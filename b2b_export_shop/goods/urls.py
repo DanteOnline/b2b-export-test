a0b6d2c949a803ec355487c6e5f3ca9dbafd9507
DanteOnline <iamdanteonline@gmail.com>
2016-08-31 02:21:57 +0300

    ajax grid-list

--- b2b_export_shop/goods/urls.py
+++ b2b_export_shop/goods/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from goods.views import GoodsView, GoodView
+from goods.views import GoodsView, GoodView, change_view
 
 urlpatterns = [
     url(r'^$', GoodsView.as_view(), name = 'goods'),
     url(r'(?P<pk>\d+)/detail/$', GoodView.as_view(), name='good'),
+    url(r'change_view/$', change_view, name='change_view'),
 ]
--- b2b_export_shop/goods/views.py
+++ b2b_export_shop/goods/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from goods.models import Good
+from django.http import HttpResponse
 
 # Create your views here.
 class GoodsView(ListView):
     model = Good
     template_name = 'goods.html'
     paginate_by = 10
+
+    def get(self, request, *args, **kwargs):
+        self.request.session.get('view', 'grid')
+        return super(GoodsView,self).get(request,*args,**kwargs)
 
     def get_queryset(self):
         goods = Good.objects.all()
@@ -18,3 +23,10 @@ class GoodsView(ListView):
 class GoodView(DetailView):
     model = Good
     template_name = 'good.html'
+
+def change_view(request):
+    if request.method == "GET":
+        request.session['view'] = request.GET['view']
+        return HttpResponse('ok', content_type='text/html')
+    else:
+        return HttpResponse('no', content_type='text/html')
